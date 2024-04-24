c7602aee0e014a33ded82f9444acf57e9455fecd
Xenia Golbraykh <karzerin@gmail.com>
2024-04-24 22:19:30 +0300

    gulp

--- les-gulp/gulpfile.js
+++ les-gulp/gulpfile.js
@@ -1,6 +1,23 @@
 const gulp = require('gulp');
-gulp.task('hello', (callback) => {
-    console.log('hello from gulp')
+gulp.task('first', (callback) => {
+    console.log('hello from gulp 1')
+    callback();
+});
+gulp.task('second', (callback) => {
+    console.log('hello from gulp 2')
+    callback();
+});
+gulp.task('third', (callback) => {
+    console.log('hello from gulp 3')
+    callback();
+});
+gulp.task('fourth', (callback) => {
+    console.log('hello from gulp 4')
     callback();
 });
 
+// gulp.task('default', gulp.series('first', 'second', 'third')); //последовательное выполнение задач
+// gulp.task('default', gulp.parallel('first', 'second', 'third')); //одновременный запуск задач
+
+
+gulp.task('default', gulp.series('first', gulp.parallel('second', 'third'), 'fourth')); 
